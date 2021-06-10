8965dc12b1dc8f01f2a21c56f1afad87fe701466
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-06-10 23:16:20 +0300

    first commit

---
+++ C/try.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+// node *list= NULL;
+// node *n= malloc(sizeof(node));
+
+// if(n != NULL)
+// {
+//     (*n).number=1;
+// }
+
+
+int main(){
+
+
+    return 0;
+}
