b63b4530a42330f7b53aba829dbe50679860fea1
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-06-16 23:41:21 +0300

    Linked list Commit

---
+++ Java/LinkedList/Node.java
@@ -0,0 +1,6 @@
+package Java.LinkedList;
+
+public class Node {
+    int data;
+    Node next;
+}
---
+++ Java/LinkedList/Runner.java
@@ -0,0 +1,16 @@
+package Java.LinkedList;
+
+
+public class Runner {
+    public static void main(String[] args){
+        LinkedList list= new LinkedList();
+
+        list.insert(5);
+        list.insert(45);
+        list.insert(12);
+        list.insert(85);
+
+        list.show();
+        // list.add(3,12);
+    }
+}
